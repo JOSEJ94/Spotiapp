1cbdb1e80f633e0564fba97b7403b09eb47e39e3
JOSEJ94 <josejrojasc@hotmail.es>
2019-04-24 20:33:03 -0600

    final Update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,12 +9,22 @@ import { SpotifyService } from "./../../services/spotify.service";
 export class HomeComponent {
   newSongs: any[] = [];
   loading: boolean = false;
+  error: boolean = false;
+  errorMessage: string;
 
   constructor(private _spotifyService: SpotifyService) {
     this.loading = true;
-    this._spotifyService.getNewReleases().subscribe((data: any) => {
-      this.newSongs = data;
-      this.loading = false;
-    });
+    this._spotifyService.getNewReleases().subscribe(
+      (data: any) => {
+        this.newSongs = data;
+        this.loading = false;
+      },
+      err => {
+        this.error = true;
+        this.errorMessage = err.error.error.message;
+        this.loading = false;
+        console.log(err);
+      }
+    );
   }
 }
